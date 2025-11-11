252dcccc1b118dee3f9afc5fc5c2326ba0708669
Pratiti Paul <paulpratiti23@gmail.com>
2025-11-11 10:41:36 +0530

    minor changes

--- frontend/src/pages/Welcome.js
+++ frontend/src/pages/Welcome.js
@@ -1,21 +1,13 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-
+import "./Welcome.css";
 function Welcome() {
   const location = useLocation();
   const name = location.state?.name || "User";
 
   return (
-    <div style={{
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      height: "100vh",
-      fontFamily: "Poppins, sans-serif",
-      color: "#eaeaea",
-      background: "linear-gradient(135deg, #2c5364, #203a43, #0f2027)"
-    }}>
-      <h1>Welcome {name}</h1>
+    <div className="welcome-container">
+        <h1>Welcome {name}</h1>
     </div>
   );
 }
